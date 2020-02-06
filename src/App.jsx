8603c9adadd6257e4d6b9bb93de02c7fa3c76368
darrylgit <darrylsmithup@gmail.com>
2020-02-06 14:31:46 -0500

    Build for Github pages

--- src/App.jsx
+++ src/App.jsx
@@ -30,6 +30,7 @@ class App extends React.Component {
     );
     return (
       <div className="App">
+        <h1>Monsters Rolodex</h1>
         <SearchBox
           placeholder="Search monsters..."
           handleChange={this.handleChange}
