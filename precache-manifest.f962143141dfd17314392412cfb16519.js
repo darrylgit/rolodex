aebbf6449d52d74d4a097315e9ed0b6e39ef2a91
darrylgit <darrylsmithup@gmail.com>
2020-02-06 14:31:14 -0500

    Updates

---
+++ precache-manifest.f962143141dfd17314392412cfb16519.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7278cb9e20887f2a8f22dca9d4e487f2",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "442668dfed5c7f6d51ee",
+    "url": "/monsters-rolodex/static/css/main.4f4c1014.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "442668dfed5c7f6d51ee",
+    "url": "/monsters-rolodex/static/js/main.30aa3560.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
